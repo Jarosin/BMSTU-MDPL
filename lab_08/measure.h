e7ac617880d504a8e449dcf5dc5f3fee944400d4
Spirin Mihail <misha_spirin_90@mail.ru>
2023-05-05 00:29:59 +0300

    add lab_08

---
+++ lab_08/measure.h
@@ -0,0 +1,15 @@
+#ifndef MEASURE_H
+#define MEASURE_H
+
+#include <iostream>
+#include <sys/time.h>
+
+#include "float_32.h"
+#include "double_64.h"
+#include "sinus.h"
+
+void measure_arif();
+
+void measure_sin();
+
+#endif
